cd0185b9b63590117618e968a8defc36e2470064
Anthony Jones <anthony15jones@gmail.com>
2023-04-29 11:37:18 -0500

    parcial 2

--- parcial.2.py
+++ parcial.2.py
@@ -4,16 +4,20 @@ import pandas as pd
 app = Flask(__name__)
 
 df = pd.read_excel('data.xlsx')
+
 records = df.to_dict(orient='records')
+
 
 @app.route('/records', methods=['GET'])
 def get_records():
     country = 'Panama'
     indicator = 'Indicator'
 
-    filtered_records = [record for record in records if record['Country Name'] == country and record['Indicator'] == indicator]
+    filtered_records = [record for record in records if
+                        record['Country Name'] == country and record['Indicator'] == indicator]
 
     return jsonify(filtered_records)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
